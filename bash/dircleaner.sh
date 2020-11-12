cada59435d0b3b06ff6854d7db828a2de5cd2111
Timedropper <2697446335@qq.com>
2020-11-12 18:45:53 +0800

    update

--- bash/dircleaner.sh
+++ bash/dircleaner.sh
@@ -1,12 +1,6 @@
-echo '确认清理(y/n)'
-read key
-if [ $key != 'y' ]
-then
-	exit
-fi
 shopt -s extglob
 cd /sdcard/
 rm -rf !(Android|DCIM|Download|Pictures|MT2)
-rm -rf .* > /dev/null
-cd ~-
+rm -rf .* &> /dev/null
 shopt -u extglob
+cd ~-
